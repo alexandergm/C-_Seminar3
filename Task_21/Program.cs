06a26fa1ef2b37fb28f16d7e89e2b5105ce1e037
alexandergm <sasha_261@outlook.com>
2022-08-26 02:12:54 +0300

    Complete_Task21

---
+++ Task_21/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] firstDot = new int[3];
+int[] secondDot = new int[3];
+var a = firstDot;
+
+string DistanceGet()
+{
+    for(int j = 0; j < 2; j++)
+    {    
+        for(int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine($"Введите {i + 1}-й элемент {j + 1}-й точки");
+            a[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        a = secondDot;
+    }
+    double distance = Math.Round(Math.Sqrt(Math.Pow(secondDot[0] - firstDot[0],2)+
+                                            Math.Pow(secondDot[1] - firstDot[1],2)+
+                                            Math.Pow(secondDot[2] - firstDot[2],2)),2);
+    return $"Расстояние между 2мя точками в 3Д пространстве = {distance}";    
+}
+try{
+    Console.WriteLine(DistanceGet());
+}
+catch{
+    Console.WriteLine("Не вверный ввод. Нужно вводить координаты целыми числами");
+}
+
