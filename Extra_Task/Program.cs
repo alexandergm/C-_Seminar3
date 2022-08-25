6fa98c2e25c5fefefcf8c7e3579bb1c4dce594f0
alexandergm <sasha_261@outlook.com>
2022-08-26 02:38:56 +0300

    Complete Extra_Task

---
+++ Extra_Task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = N;
+double sqr = Math.Pow(N, 2);
+
+while(sum != 0)
+{
+    Console.WriteLine("Введите число: ");
+    N = Convert.ToInt32(Console.ReadLine());
+    sum += N;
+    sqr = sqr + Math.Pow(N, 2);
+    if(sum == N) break;
+}
+Console.WriteLine("Сумма квадратов введеных чисел = " + sqr);
