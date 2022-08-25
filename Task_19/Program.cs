ce212c8a782b3066cc96815c682a19bad85a43bd
alexandergm <sasha_261@outlook.com>
2022-08-25 23:33:20 +0300

    Complete Task_19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+result = RevertNum(number);
+Palynodrome();
+
+int RevertNum(int count)
+{
+    while (count > 0) {
+        result *= 10; 
+        result += count % 10; 
+        count /= 10; 
+    }
+    return result;
+}
+void Palynodrome()
+{
+    if (number == result)
+    {
+        Console.WriteLine("Число " + number + " является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число " + number + " не является палиндромом");
+    }
+}
+
+
